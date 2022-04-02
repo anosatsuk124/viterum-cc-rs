7d255880de13b9b6b39bb461993e149be71615d5
Satsuki Akiba <anosatsuk124@gmail.com>
2022-04-02 07:03:44 +0000

    first compiler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    if std::env::args().len() != 2 {
+        eprintln!("The number of arguments are not collect.");
+        std::process::exit(1)
+    }
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    println!(
+        "  mov rax, {}",
+        std::env::args().collect::<Vec<String>>()[1]
+            .parse::<u32>()
+            .unwrap()
+    );
+    println!("  ret");
+    std::process::exit(0)
 }
