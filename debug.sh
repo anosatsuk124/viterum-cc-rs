a9e144139422c76fe96d92b41ac0cf53c44b71d8
Satsuki Akiba <anosatsuk124@gmail.com>
2022-04-02 12:24:27 +0000

    implement add and sub

--- debug.sh
+++ debug.sh
@@ -1,27 +1,15 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
+expected="$1"
+input="$2"
 
-assert() {
-    expected="$1"
-    input="$2"
+./tmp/tmp
+actual="$?"
 
-    if [ ! -d ./tmp ]; then
-        mkdir ./tmp
-    fi
-
-    cargo run --release -- "$input" > ./tmp/tmp.S
-    cc -o ./tmp/tmp ./tmp/tmp.S
-    ./tmp/tmp
-    actual="$?"
-
-    if [ "$actual" = "$expected" ]; then
-        echo "$input => $actual"
-    else
-        echo "$input => $expected expected, but got $actual"
-        exit 1
-    fi
-}
-
-assert 0 0
-assert 42 42
-
-echo OK
+if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+    echo OK
+    exit 0
+else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+fi
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
+use std::iter::Peekable;
+
+fn strtou32<T: Iterator<Item = char>>(iter: &mut Peekable<T>, dig: u32) -> u32 {
+    let mut val: u32 = 0;
+
+    while let Some(pos) = iter.peek() {
+        match pos.to_digit(dig) {
+            Some(num) => val = val * dig + num,
+            None => break,
+        }
+        iter.next();
+    }
+
+    return val;
+}
+
 fn main() {
-    if std::env::args().len() != 2 {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
         eprintln!("The number of arguments are not collect.");
         std::process::exit(1)
     }
+
+    let mut expr = args[1].chars().peekable();
+
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
-    println!(
-        "  mov rax, {}",
-        std::env::args().collect::<Vec<String>>()[1]
-            .parse::<u32>()
-            .unwrap()
-    );
+    println!("  mov rax, {}", strtou32(&mut expr, 10));
+
+    while let Some(c) = expr.next() {
+        match c {
+            '+' => println!("  add rax, {}", strtou32(&mut expr, 10)),
+            '-' => println!("  sub rax, {}", strtou32(&mut expr, 10)),
+            _ => eprintln!("Invalid charactor: '{}'", c),
+        }
+    }
     println!("  ret");
-    std::process::exit(0)
 }
